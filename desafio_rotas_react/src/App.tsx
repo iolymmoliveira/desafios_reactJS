51c296d46304b3f3a381ef8312824ecc3354c05b
ioly Oliveira <iolymmoliveira@gmail.com>
2025-04-29 15:06:15 -0300

    Added Links without full page reload

--- desafio_rotas_react/src/App.tsx
+++ desafio_rotas_react/src/App.tsx
@@ -1,9 +1,18 @@
-import Home from "./routes/Home";
+// import Home from "./routes/Home";
+
+import { Link } from "react-router-dom"
 
 function App() {
   return (
     <>
-      <Home />
+      <nav style={{
+        borderBottom: "solid 1px",
+        paddingBottom: "1rem",
+      }}>
+        <Link to="/">Início</Link>
+        <Link to="/products">Produtos</Link>
+        <Link to="/about">Sobre nós</Link>
+      </nav>
     </>
   )
 }
