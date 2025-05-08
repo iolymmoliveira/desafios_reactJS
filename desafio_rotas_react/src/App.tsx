a1c88c7c0b17c715eadcc8e0079595f989756dce
ioly Oliveira <iolymmoliveira@gmail.com>
2025-05-08 20:24:15 -0300

    Adjustments in routes

--- desafio_rotas_react/src/App.tsx
+++ desafio_rotas_react/src/App.tsx
@@ -1,20 +1,49 @@
-// import Home from "./routes/Home";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import Products from "./routes/Products";
+import Books from "./routes/Products/Books";
+import Computers from "./routes/Products/Computers";
+import Electronics from "./routes/Products/Electronics";
+import About from "./routes/About";
+import NotFound from "./routes/NotFound";
+import PageContent from "./routes/HomeContent";
 
-import { Link } from "react-router-dom"
+const textLeft = "Não";
+const textRight = "encontrado!";
 
 function App() {
   return (
     <>
-      <nav style={{
-        borderBottom: "solid 1px",
-        paddingBottom: "1rem",
-      }}>
-        <Link to="/">Início</Link>
-        <Link to="/products">Produtos</Link>
-        <Link to="/about">Sobre nós</Link>
-      </nav>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route index element={<Navigate to="/home" />} />
+            <Route path="home" element={<PageContent title="Bem Vindos!!!" />} />
+            <Route path="about" element={<About />} />
+
+            <Route path="products" element={<Products />}>
+              <Route path="books" element={<Books />} />
+              <Route path="computers" element={<Computers />} />
+              <Route path="electronics" element={<Electronics />} />
+            </Route>
+
+            <Route
+              path="*"
+              element={
+                <NotFound
+                  url={
+                    "https://cdn.svgator.com/images/2024/04/cat-revenge-animation-404-page.gif"
+                  }
+                  textLeft={textLeft}
+                  textRight={textRight}
+                />
+              }
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- desafio_rotas_react/src/main.tsx
+++ desafio_rotas_react/src/main.tsx
@@ -1,10 +1,12 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-)
+createRoot(document.getElementById("root")!).render(
+
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+
+);
