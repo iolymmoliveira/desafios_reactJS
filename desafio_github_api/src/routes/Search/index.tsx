bab4f6519463317ed88d6a43eeb8a74c0df8a352
ioly Oliveira <iolymmoliveira@gmail.com>
2025-08-14 21:46:24 -0300

    Add page search

--- desafio_github_api/src/routes/Search/index.tsx
+++ desafio_github_api/src/routes/Search/index.tsx
@@ -1,7 +1,12 @@
+import './style.css';
+import Button from '../../components/Button';
+
 export default function Search() {
   return (
-    <div>
-      <h1>Search Component</h1>
+    <div id="dsgh-search" className="dsgh-search-container">
+      <h1 className="dsgh-search-title">Encontre um perfil</h1>
+      <input type="text" className="dsgh-search-input" placeholder="Usuário Github" />
+      <Button textButton="Encontrar" />
     </div>
   );
 }
