83333a7bc5f2255d8cf5d6a2afaf1e40e3e4e3f6
ioly Oliveira <iolymmoliveira@gmail.com>
2025-11-21 16:56:10 -0300

    implement GitHub user search functionality

--- desafio_github_api/src/routes/Search/index.tsx
+++ desafio_github_api/src/routes/Search/index.tsx
@@ -1,12 +1,62 @@
 import './style.css';
 import Button from '../../components/Button';
+import { useState } from 'react';
+import axios from 'axios';
+import type { userDTO } from '../../models/userDTO';
+
+
+type InputName = {
+  name: string;
+}
 
 export default function Search() {
+
+  const [inputName, setInputName] = useState<InputName>({name: ""});
+  const [userData, setUserData] = useState<userDTO>();
+
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    event.preventDefault();
+    setInputName({name: event.target.value});
+  }
+
+  function handleSearch() {
+    if (!inputName) return;
+
+    axios.get(`https://api.github.com/users/${inputName.name}`)
+    .then(response => {
+      console.log(response.data);
+      setUserData(response.data);
+    })
+    .catch(error => {
+      setUserData(undefined);
+      console.error('Error searching user:', error);
+    });
+  };
+
+
   return (
-    <div id="dsgh-search" className="dsgh-search-container">
-      <h1 className="dsgh-search-title">Encontre um perfil</h1>
-      <input type="text" className="dsgh-search-input" placeholder="Usuário Github" />
-      <Button textButton="Encontrar" />
-    </div>
+    <>
+      <div id="dsgh-search" className="dsgh-search-container">
+        <h1 className="dsgh-search-title">Encontre um perfil</h1>
+        <input type="text" className="dsgh-search-input" placeholder="Usuário Github" value={inputName.name} onChange={handleInputChange} />
+        <Button textButton="Encontrar" onClick={handleSearch} />
+      </div>
+      <div>
+        {userData && (
+          <div className="dsgh-user-info">
+            <div className="dsgh-user-img">
+              <img src={userData.avatar_url} alt="Avatar" width={80} />
+            </div>  
+            <div className="dsgh-user-data">
+              <h3>Informações</h3>
+              <p>Perfil: {userData.url}</p>
+              <p>Seguidores: {userData.followers}</p>
+              <p>Localidade: {userData.location}</p>
+              <p>Nome: {userData.name}</p>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
