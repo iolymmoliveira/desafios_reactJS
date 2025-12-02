eb5899573090de2c11d01907812dff2c093f3811
ioly Oliveira <iolymmoliveira@gmail.com>
2025-12-02 11:54:27 -0300

    Implement product filtering functionality in ListingBody

--- desafio_filter/src/assets/components/ListingBody/index.tsx
+++ desafio_filter/src/assets/components/ListingBody/index.tsx
@@ -1,12 +1,49 @@
-import './style.css';
+import "./style.css";
 import Filter from "../Filter";
-import Listing from '../Listing';
+import Listing from "../Listing";
+import type { ProductDTO } from "../../../models/product";
+import { findByPrice } from "../../../services/product-service";
+import { useState, useEffect } from "react";
+
+type QueryParams = {
+  minPrice: number;
+  maxPrice: number;
+};
 
 export default function ListingBody() {
+  const [products, setProducts] = useState<ProductDTO[]>([]);
+
+  const [queryParams, setQueryParams] = useState<QueryParams>({
+    minPrice: 0,
+    maxPrice: Number.MAX_VALUE,
+  });
+
+  function handleFilter(min: number, max: number) {
+    setProducts([]);
+    setQueryParams({
+      ...queryParams,
+      minPrice: min,
+      maxPrice: max,
+    });
+  }
+
+  useEffect(() => {
+    const newProducts = findByPrice(queryParams.minPrice, queryParams.maxPrice);
+    setProducts(newProducts);
+  }, [queryParams]);
+
   return (
     <div className="dsf-listing-body-container">
-      <Filter />
-      <Listing/>
+      <Filter onFilter={handleFilter} />
+      { products.length === 0 ? (
+        <h2>Nenhum produto encontrado</h2>
+      ) : (
+        <ul className="dsf-listing-body-items">
+          {products.map((product) => (
+            <Listing key={product.id} product={product} />
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
