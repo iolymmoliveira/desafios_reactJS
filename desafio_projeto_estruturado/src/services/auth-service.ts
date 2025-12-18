7bfe9b52155f846a0e9b2dd211205c0b7510bca8
ioly Oliveira <iolymmoliveira@gmail.com>
2025-12-18 16:04:39 -0300

    feat: Add hasAnyRoles

--- desafio_projeto_estruturado/src/services/auth-service.ts
+++ desafio_projeto_estruturado/src/services/auth-service.ts
@@ -1,5 +1,5 @@
 import QueryString from "qs";
-import type { AccessTokenPayloadDTO, CredentialsDTO } from "../models/auth";
+import type { AccessTokenPayloadDTO, CredentialsDTO, RoleEnum } from "../models/auth";
 import { CLIENT_ID, CLIENT_SECRET } from "../utils/system";
 import type { AxiosRequestConfig } from "axios";
 import { requestBackend } from "../utils/requests";
@@ -53,4 +53,22 @@ export function getAccessTokenPayload(): AccessTokenPayloadDTO | undefined {
 export function isAuthenticated(): boolean {
   let tokenPayload = getAccessTokenPayload();
   return tokenPayload && tokenPayload.exp * 1000 > Date.now() ? true : false;
+}
+
+export function hasAnyRoles(roles: RoleEnum[]): boolean {
+  if (roles.length === 0) {
+    return true;
+  }
+
+  const tokenPayload = getAccessTokenPayload();
+
+  if (tokenPayload !== undefined) {
+    for (let i = 0; i < roles.length; i++) {
+      if (tokenPayload.authorities.includes(roles[i])) {
+        return true;
+      }
+    }
+  }
+
+  return false;
 }
