1307ca4e6daa0512bdae6249d2a2c1d5c1aa0778
ioly Oliveira <iolymmoliveira@gmail.com>
2025-12-18 15:06:47 -0300

    feat: Implements PrivateRoute

--- desafio_projeto_estruturado/src/App.tsx
+++ desafio_projeto_estruturado/src/App.tsx
@@ -1,28 +1,34 @@
-import "./index.css"
-import { Navigate, Route, Routes } from "react-router-dom"
-import ClientHome from "./routes/ClientHome"
-import Login from "./routes/ClientHome/Login"
-import AdminHome from "./routes/Admin/AdminHome"
-import Admin from "./routes/Admin"
+import "./index.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+import ClientHome from "./routes/ClientHome";
+import Login from "./routes/ClientHome/Login";
+import AdminHome from "./routes/Admin/AdminHome";
+import Admin from "./routes/Admin";
 import { unstable_HistoryRouter as HistoryRouter } from "react-router-dom";
 import { history } from "./utils/history";
+import { PrivateRoute } from "./components/PrivateRoute";
 
 function App() {
-
   return (
-    <HistoryRouter history={history}>    
+    <HistoryRouter history={history}>
       <Routes>
-        <Route path="/" element={<ClientHome />} >
+        <Route path="/" element={<ClientHome />}>
           <Route path="/" element={<Login />} />
         </Route>
         <Route path="*" element={<Navigate to="/" />} />
-        <Route path="/admin/" element={<Admin />}>
+        <Route
+          path="/admin/"
+          element={
+            <PrivateRoute>
+              <Admin />
+            </PrivateRoute>
+          }
+        >
           <Route index element={<AdminHome />} />
-
         </Route>
       </Routes>
     </HistoryRouter>
-  )
+  );
 }
 
-export default App
+export default App;
