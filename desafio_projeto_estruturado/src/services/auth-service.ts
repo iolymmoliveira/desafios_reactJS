d19d459bbb70ac70e92b4d3791ee28f2ae8b4e12
ioly Oliveira <iolymmoliveira@gmail.com>
2025-12-18 13:13:49 -0300

    feat(auth): implement JWT decoding and authentication helpers

--- desafio_projeto_estruturado/src/services/auth-service.ts
+++ desafio_projeto_estruturado/src/services/auth-service.ts
@@ -1,28 +1,28 @@
 import QueryString from "qs";
-import type { CredentialsDTO } from "../models/auth";
+import type { AccessTokenPayloadDTO, CredentialsDTO } from "../models/auth";
 import { CLIENT_ID, CLIENT_SECRET } from "../utils/system";
 import type { AxiosRequestConfig } from "axios";
 import { requestBackend } from "../utils/requests";
 import * as accessTokenRepository from "../localstorage/access-token-repository";
+import jwtDecode from "jwt-decode";
 
 export function loginRequest(loginData: CredentialsDTO) {
-
   const headers = {
     "Content-type": "application/x-www-form-urlencoded",
-    "Authorization": "Basic " + window.btoa(CLIENT_ID + ":" + CLIENT_SECRET),
-  }
+    Authorization: "Basic " + window.btoa(CLIENT_ID + ":" + CLIENT_SECRET),
+  };
 
   const requestBody = QueryString.stringify({
     ...loginData,
     grant_type: "password",
-  })
+  });
 
-  const config : AxiosRequestConfig = {
+  const config: AxiosRequestConfig = {
     method: "POST",
     url: "/oauth/token",
     data: requestBody,
     headers,
-  }
+  };
 
   return requestBackend(config);
 }
@@ -37,4 +37,20 @@ export function saveAccessToken(token: string) {
 
 export function getAccessToken() {
   return accessTokenRepository.get();
+}
+
+export function getAccessTokenPayload(): AccessTokenPayloadDTO | undefined {
+  try {
+    const token = accessTokenRepository.get();
+    return token == null
+      ? undefined
+      : (jwtDecode(token) as AccessTokenPayloadDTO);
+  } catch (error) {
+    return undefined;
+  }
+}
+
+export function isAuthenticated(): boolean {
+  let tokenPayload = getAccessTokenPayload();
+  return tokenPayload && tokenPayload.exp * 1000 > Date.now() ? true : false;
 }
