1f0d06ad228a08042597d849dc866850d86feaec
ioly Oliveira <iolymmoliveira@gmail.com>
2025-12-22 18:00:21 -0300

    feat: Add reusable Button component with variants

---
+++ desafio_projeto_estruturado/src/components/Button/index.tsx
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+type ButtonProps = {
+  text?: string;
+  onClick?: () => void;
+  variant?: "primary" | "secondary";
+  className?: string;
+};
+
+export default function Button({ text, onClick, variant, className = "" }: ButtonProps) {
+  return (
+    <div 
+      className={`dsc-button ${
+        variant ? `dsc-button-${variant}` : `${className}`
+      }`}
+    >
+      {text}
+    </div>
+  );
+}
