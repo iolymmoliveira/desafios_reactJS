8bdd292d1be7bda4edada3f291d8ea5a3a603586
ioly Oliveira <iolymmoliveira@gmail.com>
2025-12-23 13:02:52 -0300

    refactor: convert ProductDetails to dynamic ProductDetailsCard with props

---
+++ desafio_projeto_estruturado/src/components/ProductDetailsCard/index.tsx
@@ -0,0 +1,33 @@
+import "./styles.css";
+import type { ProductDTO } from "../../models/product";
+import ProductCategory from "../ProductCategory";
+
+type Props = {
+  product: ProductDTO,
+}
+
+export default function ProductDetailsCard({ product } : Props) {
+  return (
+    <>
+      <div className="dsc-card">
+        <div className="dsc-card-top">
+          <img src={product.imgUrl} alt={product.name} />
+        </div>
+        <div className="dsc-card-bottom">
+          <h3>R$ {product.price.toFixed(2)}</h3>
+          <h4>{product.name}</h4>
+          <p>{product.description}</p>
+          <div className="dsc-category-container">
+            {
+              product.categories.map(
+                item => (
+                  <ProductCategory key={item.id} name={item.name} />
+                )
+              )
+            }
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
