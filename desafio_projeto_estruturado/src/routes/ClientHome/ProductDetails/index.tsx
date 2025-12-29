61d9936087c405caca1c4100c8375419bf4ab3b1
ioly Oliveira <iolymmoliveira@gmail.com>
2025-12-29 12:41:30 -0300

    refactor: handle request errors in product service

--- desafio_projeto_estruturado/src/routes/ClientHome/ProductDetails/index.tsx
+++ desafio_projeto_estruturado/src/routes/ClientHome/ProductDetails/index.tsx
@@ -2,29 +2,32 @@ import "./styles.css";
 import Button from "../../../components/Button";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
 import * as productService from "../../../services/product-service";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import type { ProductDTO } from "../../../models/product";
 
 export default function ProductDetails() {
   const params = useParams();
-
   const [product, setProduct] = useState<ProductDTO>();
+  const navigate = useNavigate();
 
   useEffect(() => {
     productService.findById(Number(params.productId))
       .then(response => {
         setProduct(response.data);
       })
-      .catch(error => {
-        console.log("Error: ", error);
+      .catch(() => {
+        navigate("/");
       })
   }, []);
 
   return (
     <main>
       <section id="dsc-product-details-section" className="dsc-container">
-        {product && <ProductDetailsCard product={product} />}
+        {
+          product && 
+          <ProductDetailsCard product={product} />
+        }
         <div className="dsc-button-container">
           <Button text="Comprar" variant="primary" />
           <Link to="/">
