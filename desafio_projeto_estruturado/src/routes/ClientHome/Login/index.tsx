420ae180eb5d60d981a79a600e6b6c014c480dca
ioly Oliveira <iolymmoliveira@gmail.com>
2026-01-27 16:37:34 -0300

    feat: Custom FormInput

--- desafio_projeto_estruturado/src/routes/ClientHome/Login/index.tsx
+++ desafio_projeto_estruturado/src/routes/ClientHome/Login/index.tsx
@@ -1,37 +1,56 @@
 import "./styles.css";
 import { useContext, useState } from "react";
-import type { CredentialsDTO } from "../../../models/auth";
 import * as authService from "../../../services/auth-service";
 import { useNavigate } from "react-router-dom";
 import { ContextToken } from "../../../utils/context-token";
+import FormInput from "../../../components/FormInput";
 
 export default function Login() {
-
-  const [formData, setFormData] = useState<CredentialsDTO>({
-    username: '',
-    password: '',
-  })
+  const [formData, setFormData] = useState<any>({
+    username: {
+      value: "",
+      id: "username",
+      name: "username",
+      type: "text",
+      placeholder: "Email",
+      validation: function (value: string) {
+        return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+          value.toLowerCase(),
+        );
+      },
+      message: "Favor informar um email válido",
+    },
+    password: {
+      value: "",
+      id: "password",
+      name: "password",
+      type: "password",
+      placeholder: "Senha",
+    },
+  });
 
   const navigate = useNavigate();
   const { setContextTokenPayload } = useContext(ContextToken);
 
   function handleSubmit(event: any) {
     event.preventDefault();
-    authService.loginRequest(formData)
-      .then(response => {
+    authService
+      .loginRequest({username: formData.username.value, password: formData.password.value})
+      .then((response) => {
         authService.saveAccessToken(response.data.access_token);
         setContextTokenPayload(authService.getAccessTokenPayload());
         navigate("/cart");
       })
-      .catch(error => {
+      .catch((error) => {
         console.log("Erro no login: ", error);
-      })
+      });
   }
 
   function handleInputChange(event: any) {
     const value = event.target.value;
     const name = event.target.name;
-    setFormData({...formData, [name]: value});
+    setFormData({ ...formData, [name]: {...formData[name], value: value}
+     });
   }
 
   return (
@@ -42,34 +61,30 @@ export default function Login() {
             <h2>Login</h2>
             <div className="dsc-form-controls-container">
               <div>
-                <input 
-                  name="username"
-                  value={formData.username}
-                  type="text" 
-                  className="dsc-form-control dsc-input-error" 
-                  placeholder="Email"
+                <FormInput
+                  { ...formData.username }
+                  className="dsc-form-control dsc-input-error"
                   onChange={handleInputChange}
                 />
                 <div className="dsc-form-error">Campo obrigatório</div>
               </div>
               <div>
-                <input 
-                  name="password"
-                  value={formData.password}
-                  type="password"
+                <FormInput
+                  { ...formData.password }
                   className="dsc-form-control"
-                  placeholder="Senha"
                   onChange={handleInputChange}
                 />
               </div>
             </div>
 
             <div className="dsc-login-form-buttons">
-              <button type="submit" className="dsc-button dsc-button-primary">Entrar</button>
+              <button type="submit" className="dsc-button dsc-button-primary">
+                Entrar
+              </button>
             </div>
           </form>
         </div>
       </section>
     </main>
-  )
+  );
 }
